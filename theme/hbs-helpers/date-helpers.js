64600463c5b361ab05517ac2f97f234519a80af5
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2025-02-05 22:26:55 +0100

    🐛 fix: correct date format

--- theme/hbs-helpers/date-helpers.js
+++ theme/hbs-helpers/date-helpers.js
@@ -6,7 +6,7 @@ moment.locale('fr'); // Set locale to French
 const dateHelpers = {
   MY: date => moment(date.toString(), ['YYYY-MM-DD']).format('MMM YYYY'),
   Y: date => moment(date.toString(), ['YYYY-MM-DD']).format('YYYY'),
-  DMY: date => moment(date.toString(), ['DD-MM-YYYY']).format('D MMM YYYY')
+  DMY: date => moment(date.toString(), ['YYYY-MM-DD']).format('D MMM YYYY')
 };
 
 module.exports = { dateHelpers };
